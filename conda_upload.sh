1631e69403f6e9deafa3a6fd16fa4f958c1276dc
MarcYin <marcollinbobo@gmail.com>
2019-01-28 16:04:53 +0000

    conda buid;

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,18 +1,19 @@
 conda install conda-build  
 conda install anaconda-client 
-PKG_NAME=siac && USER=f0xy  
+PKG_NAME=toa2lai && USER=f0xy  
 OS=$TRAVIS_OS_NAME-64  
 mkdir ~/conda-bld 
 conda config --set anaconda_upload no  
 export CONDA_BLD_PATH=~/conda-bld 
 export VERSION=$S2_TOA_TO_LAI_VERSION
 #conda build --output . -c conda-forge
-export CONDA_PACKAGE=`conda build --output . | grep bz2`
-echo $CONDA_PACKAGE
+#export CONDA_PACKAGE=`conda build --output . | grep bz2`
+conda build .
+#echo $CONDA_PACKAGE
 ls -lah $CONDA_BLD_PATH/$OS 
 ls -lah $CONDA_BLD_PATH
 ls -lah ~/
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $(ls $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION*.tar.bz2) --force
 
 # Only need to change these two variables
 #PKG_NAME=siac
