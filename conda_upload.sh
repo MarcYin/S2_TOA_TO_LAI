9d5bcf0100c2d8f13974ee284b2a37bda4915222
MarcYin <marcollinbobo@gmail.com>
2019-01-28 18:53:09 +0000

    find the package !

--- conda_upload.sh
+++ conda_upload.sh
@@ -7,13 +7,13 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld 
 export VERSION=$S2_TOA_TO_LAI_VERSION
 #conda build --output . -c conda-forge
-#export CONDA_PACKAGE=`conda build --output . | grep bz2`
-conda build .
+export CONDA_PACKAGE=`conda build --output . | grep bz2`
+#conda build .
 #echo $CONDA_PACKAGE
 ls -lah $CONDA_BLD_PATH/$OS 
 ls -lah $CONDA_BLD_PATH/noarch
-ls -lah ~/
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(ls $CONDA_BLD_PATH/noarch/$PKG_NAME-$VERSION*.tar.bz2) --force
+#ls -lah ~/
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $(CONDA_PACKAGE) --force
 
 # Only need to change these two variables
 #PKG_NAME=siac
